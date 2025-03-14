a5bde745e990a7eb0b4fec9ffaad2e62cf56d231
Christian Manzaraz <magnusmanz@icloud.com>
2025-03-14 12:16:13 -0300

    Initial Commit

---
+++ LocationAndPlaceLookup/LocationAndPlaceLookupApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationAndPlaceLookupApp.swift
+//  LocationAndPlaceLookup
+//
+//  Created by Christian Manzaraz on 14/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct LocationAndPlaceLookupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
